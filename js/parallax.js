0d9405f6a3f3fa90244a46fde612aef10f00c219
elys <elysdominguez2@gmail.com>
2023-01-25 14:00:41 +0100

    uploaded old project for my portfolio

---
+++ js/parallax.js
@@ -0,0 +1,47 @@
+
+(function(){
+
+
+
+// Propiedades
+
+var propParallax = {
+
+	seccion: document.querySelector('.contacto-parallax'),
+	recorrido: null,
+	limite: null
+}
+
+// Metodos
+
+
+var metParallax = {
+
+	inicio: function() {
+
+		window.addEventListener('scroll', metParallax.scrollParallax);
+	},
+
+	scrollParallax: function() {
+
+		propParallax.recorrido = window.pageYOffset;
+		propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;
+
+		if ( propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite) {
+
+			propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 1.5 + 'px';
+		
+		} else{
+			propParallax.seccion.style.backgroundPositionY = 0;
+		}
+ 
+
+	}
+
+
+}
+
+metParallax.inicio();
+
+
+}())
